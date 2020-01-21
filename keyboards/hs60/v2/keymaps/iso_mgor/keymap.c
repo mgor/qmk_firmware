40719aef4d6f0f88fb40cde55dbdc5bcae65a565
Mikael Göransson <github@mgor.se>
2020-01-21 19:09:31 +0100

    corrected F-key

--- keyboards/hs60/v2/keymaps/iso_mgor/keymap.c
+++ keyboards/hs60/v2/keymaps/iso_mgor/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_VOLU,     KC_VOLD,  KC_MUTE,                                KC_TRNS,                                KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [2] = LAYOUT_60_iso( /* Empty for dynamic keymaps */
-    KC_TRNS,     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_0,        KC_TRNS,  KC_TRNS,  KC_TRNS,\
+    KC_TRNS,     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,      KC_TRNS,  KC_TRNS,  KC_TRNS,\
     KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,  KC_TRNS,          \
     KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,\
     KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,            KC_TRNS,\
