5b17f2878360340e50a7f6571201e3e8d68e9d26
Mikael Göransson <github@mgor.se>
2020-02-08 09:44:41 +0100

    mapped KC_INS to 'z' in caps layer

--- keyboards/hs60/v2/iso/keymaps/mgor/keymap.c
+++ keyboards/hs60/v2/iso/keymaps/mgor/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,      KC_F11,   KC_F12,   KC_DEL, \
     KC_TRNS,     KC_PGUP,  KC_HOME,  KC_END,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_INS,   KC_MUTE,  KC_VOLD,     KC_VOLU,  KC_TRNS,          \
     KC_TRNS,     KC_PGDN,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_MPRV,     KC_MPLY,  KC_MNXT,  KC_TRNS,\
-    KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_CALC,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,            KC_UP,  \
+    KC_TRNS,     KC_TRNS,  KC_INS,   KC_TRNS,  KC_CALC,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,            KC_UP,  \
     KC_TRNS,     KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,     KC_LEFT,  KC_DOWN,  KC_RIGHT),
 
 [_MENU] = LAYOUT_60_iso( /* MENU */
