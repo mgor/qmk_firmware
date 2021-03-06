723b4d736f3ba4a018811f8722660f1cf26a402f
Mikael Göransson <github@mgor.se>
2021-03-06 09:37:54 +0100

    cu65: qmk changed layout name

--- keyboards/capsunlocked/cu65/keymaps/mgor/keymap.c
+++ keyboards/capsunlocked/cu65/keymaps/mgor/keymap.c
@@ -27,7 +27,7 @@ enum keyboard_layers {
 #define MG_FUNC MO(_FUNC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_65_iso(
+    [_BASE] = LAYOUT_65_iso_blocker(
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
      * │Esc│ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │ Backsp│Ins│
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP  , KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MG_FUNC,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
-    [_FUNC] = LAYOUT_65_iso(
+    [_FUNC] = LAYOUT_65_iso_blocker(
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
      * │RST│ F1│ F2│ F3│ F4│ F5│ F6│ F7│ F8│ F9│F10│F11│F12│       │   │
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______ , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,                    _______,                            _______, _______,          _______, _______, _______
     ),
-    [_CAPS] = LAYOUT_65_iso(
+    [_CAPS] = LAYOUT_65_iso_blocker(
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
      * │RST│ F1│ F2│ F3│ F4│ F5│ F6│ F7│ F8│ F9│F10│F11│F12│       │   │
