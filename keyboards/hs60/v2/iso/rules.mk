708651cec69f4732e89d9508106985adb6f13465
Mikael Göransson <github@mgor.se>
2023-04-15 19:13:24 +0200

    hs60/v3: non-wilba_tech dependant firmware implementation

--- keyboards/hs60/v2/iso/rules.mk
+++ keyboards/hs60/v2/iso/rules.mk
@@ -7,15 +7,15 @@ OPT_DEFS += -DNO_SUSPEND_POWER_DOWN
 #   change yes to no to disable
 #
 BACKLIGHT_ENABLE = no              # Enable keyboard backlight functionality
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes      	   # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes              # Mouse keys
 EXTRAKEY_ENABLE = yes              # Audio control and System control
 CONSOLE_ENABLE = no                # Console for debug
 COMMAND_ENABLE = no                # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
+NKRO_ENABLE = yes                  # Enable N-Key Rollover
 AUDIO_ENABLE = no                  # Audio output
 NO_USB_STARTUP_CHECK = no          # Disable initialization only when usb is plugged in
-#SERIAL_LINK_ENABLE = yes
+SERIAL_LINK_ENABLE = no
 TAP_DANCE_ENABLE = no
 
 CIE1931_CURVE = yes
